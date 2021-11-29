9a9880fb8d9a3e1102f35ec6c69c36498816a822
ivan <shatowkln.ivan@yandex.ru>
2021-11-30 02:23:37 +0300

    правки 1

--- my-app/src/components/LoginPopup/LoginPopup.js
+++ my-app/src/components/LoginPopup/LoginPopup.js
@@ -1,51 +1,70 @@
-import React from "react";
-import './LoginPopup.scss';
+import React, {Component} from 'react';
+// import {connect} from 'react-redux';
+// import {userPostFetch} from '../../store/actionCreators/AuthPopup';
 
-const Modal = ({active, setActive}) => {
-    return (
-        <div className={active ? "active overlay" : "overlay"} onClick={() => setActive(false)}> 
-            <div className="authorization card" onClick={e => e.stopPropagation()}> 
-                <button type="button" className="authorization__close" onClick={() => setActive(false)}></button>
-                <form className="authorization__form" method="POST">
-                    <fieldset className="authorization__form-wrap">
-                        <legend className="authorization__form-title">Вход</legend>
-                        <ul className="authorization__form-list"> 
-                            <li className="authorization__form-item"> 
-                                <label className="authorization__form-label">
-                                    Логин
-                                </label>
-                                <div className="authorization__form-input input-wrap"> 
-                                    <input className="input" type="text"/>
-                                </div>
-                            </li>
-                            <li className="authorization__form-item"> 
-                                <label className="authorization__form-label">
-                                    Пароль
-                                </label>
-                                <div className="authorization__form-input input-wrap">
-                                    <input className="input" type="password"/>
-                                    <button className="toggle-password" type="button">
-                                        <svg className="toggle-password__icon open">
-                                            <use href="../../images/sprites.svg#view-open"></use>
-                                        </svg>
-                                        <svg className="toggle-password__icon close">
-                                            <use href="../../images/sprites.svg#view-close"></use>
-                                        </svg>
-                                    </button>
-                                </div>
-                            </li>
-                        </ul>
-                        <button className="btn type-1 authorization__form-btn" type="submit">
-                            Войти
-                        </button>
-                        <a className="authorization__form-link" href="/">
-                            Забыли пароль?
-                        </a>
-                    </fieldset>
-                </form>
+class Modal extends Component {
+    // state = {
+    //     username: "",
+    //     password: "",
+    // }
+
+    // handleChange = event => {
+    //     this.setState({
+    //       [event.target.name]: event.target.value
+    //     });
+    // }
+
+    // handleSubmit = event => {
+    //     event.preventDefault()
+    //     this.props.userPostFetch(this.state)
+    // }
+
+    render () {
+        return (
+            <div className={this.props.active ? "active overlay" : "overlay"} onClick={() => this.props.setActive(false)}> 
+                <div className="authorization card" onClick={e => e.stopPropagation()}> 
+                    <button type="button" className="authorization__close" onClick={() => this.props.setActive(false)}></button>
+                    <form className="authorization__form" method="POST">
+                        <fieldset className="authorization__form-wrap">
+                            <legend className="authorization__form-title">Вход</legend>
+                            <ul className="authorization__form-list"> 
+                                <li className="authorization__form-item"> 
+                                    <label className="authorization__form-label">
+                                        Логин
+                                    </label>
+                                    <div className="authorization__form-input input-wrap"> 
+                                        <input className="input" type="text"/>
+                                    </div>
+                                </li>
+                                <li className="authorization__form-item"> 
+                                    <label className="authorization__form-label">
+                                        Пароль
+                                    </label>
+                                    <div className="authorization__form-input input-wrap">
+                                        <input className="input" type="password"/>
+                                        <button className="toggle-password" type="button">
+                                            <svg className="toggle-password__icon open">
+                                                <use href="../../images/sprites.svg#view-open"></use>
+                                            </svg>
+                                            <svg className="toggle-password__icon close">
+                                                <use href="../../images/sprites.svg#view-close"></use>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </li>
+                            </ul>
+                            <button className="btn type-1 authorization__form-btn" type="submit">
+                                Войти
+                            </button>
+                            <a className="authorization__form-link" href="/">
+                                Забыли пароль?
+                            </a>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
+} 
 
 export default Modal
--- my-app/src/components/News/News.js
+++ my-app/src/components/News/News.js
@@ -2,69 +2,69 @@ import React, { useState } from 'react';
 import './News.scss';
 
 
-function News(props) {
+function News() {
 
     const newsItems = [
         {
             id: 1,
             date: '11.11.2017',
-            title: 'test1',
+            title: 'Новость',
             description: 'В Архангельске зафиксировали вспышку коронавируса,',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 2,
             date: '11.12.2017',
-            title: 'test2',
+            title: 'Не новость',
             description: 'В Нижнем Новгороде скорректировали схему передвижения транспорта',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 3,
             date: '14.11.2017',
-            title: 'test3',
+            title: 'Желтая пресса',
             description: 'В Севастополе ослабили масочный режим после волны COVID-19',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 4,
             date: '20.11.2017',
-            title: 'test4',
+            title: 'Утка',
             description: '5 генераторов заголовков для статей, если вы устали придумывать сами',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 5,
             date: '11.01.2017',
-            title: 'test5',
+            title: 'Фейк',
             description: 'В Нижнем Новгороде объявили о начале досрочных каникул в школах и вузах',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 6,
             date: '01.11.2017',
-            title: 'test6',
+            title: 'Событие',
             description: 'На западе Москвы провели проверку из-за смертельного ДТП',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 7,
             date: '11.11.2007',
-            title: 'test7',
+            title: 'Мероприятие',
             description: 'Обескураживающих заголовков пост',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 8,
             date: '05.11.2017',
-            title: 'test8',
+            title: 'Текст',
             description: 'Громкие новостные заголовки от классиков русской литературы',
             src: 'http://placehold.it/300x200',
         },
         {
             id: 9,
             date: '21.11.2017',
-            title: 'test9',
+            title: 'Lorem Ipsum',
             description: 'Фермер из Новосибирска оплатил кредит Сбербанку навозом',
             src: 'http://placehold.it/300x200',
         },
@@ -73,7 +73,7 @@ function News(props) {
     const [newsValue, setNewsValue] = useState('');
     
     const filterdNews = newsItems.filter(correctNews => {
-        return correctNews.title.toLowerCase().includes(newsValue.toLowerCase)
+        return correctNews.title.toLowerCase().includes(newsValue.toLowerCase())
     })
     
     return (
